a62e6ac79725170fcba0065f26c8b521e2488f42
Vidhu <vidban22@gmail.com>
2020-12-27 19:04:00 -0800

    Added global g

--- app.py
+++ app.py
@@ -10,6 +10,37 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 
 
+##############################################################################
+# User signup/login/logout
+
+
+@app.before_request
+def add_user_to_g():
+    """If we're logged in, add curr user to Flask global."""
+
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+
+    else:
+        g.user = None
+
+
+def do_login(user):
+    """Log in user."""
+
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+    """Logout user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+
+##############################################################################
+# Routes
+
 @app.route("/")
 def home():
     """Render Homepage"""
