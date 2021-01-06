dab036b5f41dcda699ec8c977918a2c86bab2537
Vidhu <vidban22@gmail.com>
2021-01-06 13:21:12 -0800

    Added explore section to home-quizby page

--- static/js/app.js
+++ static/js/app.js
@@ -27,4 +27,7 @@
   $(document).on("blur", "div#new-question input#autocomplete", function () {
     $("#list-of-categories").empty();
   });
+
+  // remove flash messages
+  window.setTimeout("document.getElementById('alert').remove();", 1500);
 }
