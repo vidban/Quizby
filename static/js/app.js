ad1a345a912b55fe1e929e881c6c7ab95fd4027f
Vidhu <vidban22@gmail.com>
2021-01-05 20:25:59 -0800

    changed plain q/a to flash cards

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 $(document).on("change", "#mult_choice", function () {
-  if ($("input[id^=mult_choice]:checked").val() === "p") {
+  if ($("input[id^=mult_choice]:checked").val() === "f") {
     $("#text_answer").parent().toggle();
     $("label[id^=answer]").parent().toggle();
   } else if ($("input[id^=mult_choice]:checked").val() === "mc") {
