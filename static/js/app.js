e4e58fb5cd0d7c074275de46aa519927c641d3b6
Vidhu <vidban22@gmail.com>
2021-02-16 09:48:44 -0800

    Modify chart to line chart

--- static/js/app.js
+++ static/js/app.js
@@ -83,50 +83,77 @@
 
     chart.data = [
       {
-        "date-taken": "2/2/2021",
+        "date-taken": "02/02/2021",
         "score(%)": 50,
       },
       {
-        "date-taken": "2/5/2021",
+        "date-taken": "02/05/2021",
         "score(%)": 75,
       },
       {
-        "date-taken": "2/7/2021",
+        "date-taken": "02/07/2021",
         "score(%)": 100,
       },
       {
-        "date-taken": "2/9/2021",
+        "date-taken": "02/09/2021",
         "score(%)": 25,
       },
       {
-        "date-taken": "2/11/2021",
+        "date-taken": "02/11/2021",
         "score(%)": 75,
       },
     ];
 
-    let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-    categoryAxis.dataFields.category = "date-taken";
-    categoryAxis.renderer.grid.template.location = 0;
-    categoryAxis.renderer.minGridDistance = 30;
+    // Set input format for the dates
+    chart.dateFormatter.inputDateFormat = "MM-dd-yyyy";
 
-    categoryAxis.renderer.labels.template.adapter.add("dy", (dy, target) => {
-      if (target.dataItem && target.dataItem.index & (2 == 2)) {
-        return dy + 25;
-      }
-      return dy;
-    });
-
+    // Create axes
+    let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
     let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
-    let series = chart.series.push(new am4charts.ColumnSeries());
+    // Create series
+    let series = chart.series.push(new am4charts.LineSeries());
     series.dataFields.valueY = "score(%)";
-    series.dataFields.categoryX = "date-taken";
-    series.name = "Score";
-    series.columns.template.tooltipText = "{categoryX} : [bold]{valueY}[/]";
-    series.columns.template.fillOpacity = 0.8;
+    series.dataFields.dateX = "date-taken";
+    series.tooltipText = "{score(%)}";
+    series.strokeWidth = 2;
+    series.minBulletDistance = 15;
 
-    let columnTemplate = series.columns.template;
-    columnTemplate.strokeWidth = 2;
-    columnTemplate.strokeOpacity = 1;
+    // Drop-shaped tooltips
+    series.tooltip.background.cornerRadius = 20;
+    series.tooltip.background.strokeOpacity = 0;
+    series.tooltip.pointerOrientation = "vertical";
+    series.tooltip.label.minWidth = 40;
+    series.tooltip.label.minHeight = 40;
+    series.tooltip.label.textAlign = "middle";
+    series.tooltip.label.textValign = "middle";
+
+    // Make bullets grow on hover
+    let bullet = series.bullets.push(new am4charts.CircleBullet());
+    bullet.circle.strokeWidth = 2;
+    bullet.circle.radius = 4;
+    bullet.circle.fill = am4core.color("#fff");
+
+    let bullethover = bullet.states.create("hover");
+    bullethover.properties.scale = 1.3;
+
+    // Make a panning cursor
+    chart.cursor = new am4charts.XYCursor();
+    chart.cursor.behavior = "panXY";
+    chart.cursor.xAxis = dateAxis;
+    chart.cursor.snapToSeries = series;
+
+    // Create vertical scrollbar and place it before the value axis
+    chart.scrollbarY = new am4core.Scrollbar();
+    chart.scrollbarY.parent = chart.leftAxesContainer;
+    chart.scrollbarY.toBack();
+
+    // Create a horizontal scrollbar with previe and place it underneath the date axis
+    chart.scrollbarX = new am4charts.XYChartScrollbar();
+    chart.scrollbarX.series.push(series);
+    chart.scrollbarX.parent = chart.bottomAxesContainer;
+
+    dateAxis.start = 0.79;
+    dateAxis.keepSelection = true;
   });
 }
