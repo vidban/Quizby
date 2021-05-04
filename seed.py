13fefa0a0e1b743b141e67d724ecacd5a6491ad1
Vidhu <vidban22@gmail.com>
2021-05-04 12:46:29 -0700

    Deleted redundant code

--- seed.py
+++ seed.py
@@ -4,8 +4,3 @@ from models import db, User, Question
 
 db.drop_all()
 db.create_all()
-
-with open('generator/users.csv') as users:
-    db.session.bulk_insert_mappings(User, DictReader(users))
-
-db.session.commit()
